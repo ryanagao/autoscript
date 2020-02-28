9a7afe712f7a3ac9f461806667a352d04c433c35
Ryan Christian <ryanagao@yahoo.com>
2020-02-28 11:07:47 +0800

    edit autoscript

--- unirest_autoscript.sh
+++ unirest_autoscript.sh
@@ -1,7 +1,32 @@
+# Node version to be installed
+NODE_VERSION=lts
+
 cd
-sudo apt update
+echo "|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|"
+echo "|  EXECUTING UNIREST_AUTOSCRIPT |"
+echo "|_______________________________|"
+sudo apt-get update
+
+echo "|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|"
+echo "|  SETTING UP LANGUAGE  |"
+echo "|_______________________|"
 sudo locale-gen "en_US.UTF-8"
 sudo su
 echo -e 'LANG="en_US.UTF-8" \n LANGUAGE="en_US.UTF-8" \n LC_ALL="en_US:en_US.UTF-8" \n ' > /etc/default/locale
 exit
+
+echo "|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|"
+echo "|  INSTALLING NPM  |"
+echo "|__________________|"
+sudo apt-get install npm -y
+
+echo "|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|"
+echo "|  INSTALLING N  |"
+echo "|________________|"
+sudo npm install -g n
+
+echo "|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|"
+echo "|  INSTALLING NODE  |"
+echo "|___________________|"
+sudo n $NODE_VERSION
 
